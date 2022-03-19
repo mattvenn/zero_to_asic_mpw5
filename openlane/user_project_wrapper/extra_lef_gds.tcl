3edc522d6be76e5a957e030b2f47e3cb299d6c75
matt venn <matt@mattvenn.net>
2022-03-19 17:33:37 +0100

    wrapped teras project

--- openlane/user_project_wrapper/extra_lef_gds.tcl
+++ openlane/user_project_wrapper/extra_lef_gds.tcl
@@ -4,6 +4,7 @@ set ::env(EXTRA_LEFS) "\
 	$script_dir/../../lef/wrapped_frequency_counter.lef \
 	$script_dir/../../lef/wrapped_rgb_mixer.lef \
 	$script_dir/../../lef/wrapped_hack_soc_dffram.lef \
+	$script_dir/../../lef/wrapped_teras.lef \
 	$script_dir/../../lef/wb_bridge_2way.lef \
 	$script_dir/../../lef/wb_openram_wrapper.lef \
 	$script_dir/../../lef/sky130_sram_1kbyte_1rw1r_32x256_8.lef "
@@ -13,6 +14,7 @@ set ::env(EXTRA_GDS_FILES) "\
 	$script_dir/../../gds/wrapped_frequency_counter.gds \
 	$script_dir/../../gds/wrapped_rgb_mixer.gds \
 	$script_dir/../../gds/wrapped_hack_soc_dffram.gds \
+	$script_dir/../../gds/wrapped_teras.gds \
 	$script_dir/../../gds/wb_bridge_2way.gds \
 	$script_dir/../../gds/wb_openram_wrapper.gds \
 	$script_dir/../../gds/sky130_sram_1kbyte_1rw1r_32x256_8.gds "
--- verilog/rtl/user_project_includes.v
+++ verilog/rtl/user_project_includes.v
@@ -1,17 +1,19 @@
-// +------------+--------------------+-------------------+--------------------------------------------------------+------------------------------------------+
-// | project id | title              | author            | repo                                                   | commit                                   |
-// +------------+--------------------+-------------------+--------------------------------------------------------+------------------------------------------+
-// | 0          | Function generator | Matt Venn         | https://github.com/mattvenn/wrapped_function_generator | 9e73784d43a91d70cb1a7c9c5d42037f49ed9e67 |
-// | 1          | VGA Clock          | Matt Venn         | https://github.com/mattvenn/wrapped_vga_clock          | 6c7f12b8be62d34d4f4221e18b9408b3c5ac8fcd |
-// | 2          | Frequency counter  | Matt Venn         | https://github.com/mattvenn/wrapped_frequency_counter  | 94cd6e626492dc2f623bf06163e90a84bde553cb |
-// | 3          | RGB Mixer          | Matt Venn         | https://github.com/mattvenn/wrapped_rgb_mixer          | 2676a8904591e92613fbfadf8c7f57fdfd79b1a4 |
-// | 11         | Hack soc           | Maximo Balestrini | https://github.com/mbalestrini/wrapped_hack_soc        | 54395c53d52f253bd71b35d0a1c2049c87d31103 |
-// +------------+--------------------+-------------------+--------------------------------------------------------+------------------------------------------+
+// +------------+--------------------+----------------------------+--------------------------------------------------------+------------------------------------------+
+// | project id | title              | author                     | repo                                                   | commit                                   |
+// +------------+--------------------+----------------------------+--------------------------------------------------------+------------------------------------------+
+// | 0          | Function generator | Matt Venn                  | https://github.com/mattvenn/wrapped_function_generator | 9e73784d43a91d70cb1a7c9c5d42037f49ed9e67 |
+// | 1          | VGA Clock          | Matt Venn                  | https://github.com/mattvenn/wrapped_vga_clock          | 6c7f12b8be62d34d4f4221e18b9408b3c5ac8fcd |
+// | 2          | Frequency counter  | Matt Venn                  | https://github.com/mattvenn/wrapped_frequency_counter  | 94cd6e626492dc2f623bf06163e90a84bde553cb |
+// | 3          | RGB Mixer          | Matt Venn                  | https://github.com/mattvenn/wrapped_rgb_mixer          | 2676a8904591e92613fbfadf8c7f57fdfd79b1a4 |
+// | 11         | Hack soc           | Maximo Balestrini          | https://github.com/mbalestrini/wrapped_hack_soc        | 54395c53d52f253bd71b35d0a1c2049c87d31103 |
+// | 13         | teras              | Louis Ledoux AKA Binaryman | https://github.com/Bynaryman/wrapped_teras             | 766588bd8519682347ea15680258cd97004fc377 |
+// +------------+--------------------+----------------------------+--------------------------------------------------------+------------------------------------------+
 `include "wrapped_function_generator/wrapper.v" // 0
 `include "wrapped_vga_clock/wrapper.v" // 1
 `include "wrapped_frequency_counter/wrapper.v" // 2
 `include "wrapped_rgb_mixer/wrapper.v" // 3
 `include "wrapped_hack_soc/wrapped_hack_soc_dffram.v" // 11
+`include "wrapped_teras/wrapper.v" // 13
 // shared projects
 `include "wb_bridge/src/wb_bridge_2way.v"
 `include "wb_openram_wrapper/src/register_rw.v"
